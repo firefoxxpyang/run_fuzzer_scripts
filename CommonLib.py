1ef67480179a5be5deec5b24b1764a3b31dd561f
yangyi <yangyi@is.iscas.ac.cn>
2021-12-10 09:27:52 +0800

    add run symqemu aflpp code

--- CommonLib.py
+++ CommonLib.py
@@ -603,3 +603,227 @@ def run_qsym_fuzzer_tmux(root_directory, program_name, timeout, task_count, proc
 
 # QSYM end
 #######################################################################################################################################
+
+
+#######################################################################################################################################
+# symqemu start
+
+def get_symqemu_tmux_aflpp_command(root_directory, program_name, type, task_id, process_id):
+	afl_path            = os.path.join(root_directory, "AFLplusplus", "afl-fuzz")
+	input_directory     = os.path.join(root_directory, "input", program_name, "in")
+	output_directory    = os.path.join(root_directory, "output", program_name, str(task_id))
+	program_path        = os.path.join(root_directory, "target_bin", program_name)
+	input_type			= get_input_type(program_name)
+
+	cmd_line = ""
+	cmd_line = cmd_line + afl_path
+	cmd_line = cmd_line + " -i " + input_directory
+	cmd_line = cmd_line + " -o " + output_directory
+	if "Master" == type:
+		cmd_line = cmd_line + " -M " + "Master" + str(process_id)
+	elif "Slave" == type:
+		cmd_line = cmd_line + " -S " + "Slave" + str(process_id)
+	cmd_line = cmd_line + " -Q "
+	cmd_line = cmd_line + " -m none "
+	if "file" == input_type:
+		print("file")
+		cmd_line = cmd_line + " -- " + program_path + " @@"
+	elif "stdin" == input_type:
+		print("stdin")
+		cmd_line = cmd_line + " -- " + program_path
+
+	#print(cmd_line)
+	return cmd_line
+
+
+'''
+FunctionName:
+	get_aflplusplus_tmux_command
+
+Argument:
+	root_directory					
+	program_name					program name
+	type							Master or Slave
+	task_id
+	process_id
+
+Result:
+	cmd_line
+
+Comment:
+
+'''
+def get_symqemu_tmux_afl_command(root_directory, program_name, type, task_id, process_id):
+	afl_path            = os.path.join(root_directory, "afl-2.52b", "afl-fuzz")
+	input_directory     = os.path.join(root_directory, "input", program_name, "in")
+	output_directory    = os.path.join(root_directory, "output", program_name, str(task_id))
+	program_path        = os.path.join(root_directory, "target_bin", program_name)
+	input_type			= get_input_type(program_name)
+
+	cmd_line = ""
+	cmd_line = cmd_line + afl_path
+	cmd_line = cmd_line + " -i " + input_directory
+	cmd_line = cmd_line + " -o " + output_directory
+	if "Master" == type:
+		cmd_line = cmd_line + " -M " + "Master" + str(process_id)
+	elif "Slave" == type:
+		cmd_line = cmd_line + " -S " + "Slave" + str(process_id)
+	cmd_line = cmd_line + " -Q "
+	cmd_line = cmd_line + " -m none "
+	if "file" == input_type:
+		print("file")
+		cmd_line = cmd_line + " -- " + program_path + " @@"
+	elif "stdin" == input_type:
+		print("stdin")
+		cmd_line = cmd_line + " -- " + program_path
+
+	#print(cmd_line)
+	return cmd_line
+
+'''
+FunctionName:
+	get_aflplusplus_tmux_command
+
+Argument:
+	root_directory
+	program_name
+	power_schedule
+	step
+	task_id
+	process_id
+
+Result:
+	cmd_line
+
+Comment:
+	bin/run_qsym_afl.py -a afl-slave -o $OUTPUT -n qsym -- $QSYM_CMDLINE
+
+'''
+def get_symqemu_tmux_solver_command(root_directory, program_name, task_id, process_id ):
+	qsym_script_path    = os.path.join(root_directory, "qsym", "bin", "run_qsym_afl.py")
+	output_directory    = os.path.join(root_directory, "output", program_name, str(task_id))
+	program_path        = os.path.join(root_directory, "target_bin", program_name)
+	input_type			= get_input_type(program_name)
+
+	cmd_line = ""
+	cmd_line = cmd_line + qsym_script_path
+	cmd_line = cmd_line + " -o " + output_directory
+	cmd_line = cmd_line + " -a " + "Slave" + str(process_id - 1)
+	cmd_line = cmd_line + " -n qsym "
+	cmd_line = cmd_line + " -- " + program_path
+	
+	if "file" == input_type:
+		print("file")
+
+	elif "stdin" == input_type:
+		print("stdin")
+
+	#print(cmd_line)
+	return cmd_line
+
+'''
+FunctionName:
+	run_cxxfilt_afl_fuzzer_tmux
+
+Argument:
+	root_directory
+	program_name
+	task_count
+	process_count
+
+Result:
+
+
+Comment:
+
+'''
+def run_symqemu_fuzzer_tmux(root_directory, program_name, timeout, task_count, process_count):
+	tmux_name = program_name
+	cmd_line = ""
+
+	for i in range(0, task_count):
+		# init directory
+		output_directory = os.path.join(root_directory, "output", program_name, str(i))
+		print(output_directory)
+
+		if os.path.exists(output_directory):
+			shutil.rmtree(output_directory)
+
+		os.makedirs(output_directory)
+
+		# start tmux
+		current_window_name = program_name + "_" + str(i)
+		cmd_line ="tmux send-keys -t %s 'tmux new-window -n %s' ENTER;" % (tmux_name, current_window_name)
+		print(cmd_line)
+		os.system(cmd_line)
+		time.sleep(0.3)
+
+		cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux split-window -v' ENTER;" % (tmux_name, current_window_name)
+		print(cmd_line)
+		os.system(cmd_line)
+		time.sleep(0.3)
+
+		cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux split-window -h' ENTER;" % (tmux_name, current_window_name)
+		print(cmd_line)
+		os.system(cmd_line)
+		time.sleep(0.3)
+		
+		cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux select-pane -U' ENTER;" % (tmux_name, current_window_name)
+		print(cmd_line)
+		os.system(cmd_line)
+		time.sleep(0.3)
+		
+		cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux split-window -h' ENTER;" % (tmux_name, current_window_name)
+		print(cmd_line)
+		os.system(cmd_line)
+		time.sleep(0.3)
+
+		cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux select-pane -t 0' ENTER;" % (tmux_name, current_window_name)
+		print(cmd_line)
+		os.system(cmd_line)
+		time.sleep(0.3)
+		
+		current_window_name = program_name + "_" + str(i)
+		for j in range(0, process_count):
+			print("current i:%d \tj:%d" % ( i, j ))
+			if 0 == j:
+				# master
+				cmd_line = ""
+				fuzz_cmd_line = ""
+				fuzz_cmd_line = get_symqemu_tmux_aflpp_command(root_directory, program_name, "Master", i, j)
+				fuzz_cmd_line = "timeout " + timeout + " " + fuzz_cmd_line
+				#print(fuzz_cmd_line)
+
+				cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux select-pane -t %d && %s' ENTER;" % (tmux_name, current_window_name, j + 1, fuzz_cmd_line)
+				print(cmd_line)
+				os.system(cmd_line)
+				time.sleep(0.3)
+
+			elif 2 == j:
+				# symbolic execution
+				cmd_line = ""
+				fuzz_cmd_line = ""
+				fuzz_cmd_line = get_symqemu_tmux_solver_command(root_directory, program_name, i, j)
+				fuzz_cmd_line = "timeout " + timeout + " " + fuzz_cmd_line
+
+				cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux select-pane -t %d && %s' ENTER;" % (tmux_name, current_window_name, j + 1, fuzz_cmd_line)
+				print(cmd_line)
+				os.system(cmd_line)
+				time.sleep(0.3)
+			
+			else:
+				# other slave
+				cmd_line = ""
+				fuzz_cmd_line = ""
+				fuzz_cmd_line = cmd_line + get_symqemu_tmux_aflpp_command(root_directory, program_name, "Slave", i, j)
+				fuzz_cmd_line = "timeout " + timeout + " " + fuzz_cmd_line
+
+				cmd_line = "tmux send-keys -t %s 'tmux select-window -t %s && tmux select-pane -t %d && %s' ENTER;" % (tmux_name, current_window_name, j + 1, fuzz_cmd_line)
+				print(cmd_line)
+				os.system(cmd_line)
+				time.sleep(0.3)
+
+	print("[run_cxxfilt_fuzzer_tmux] ........ ok")
+
+# symqemu end
+#######################################################################################################################################
